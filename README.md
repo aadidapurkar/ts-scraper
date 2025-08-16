9cb70bdb828b12555c5fe3477fa42b78e0d37e1e
Aadi Dapurkar <aadidapurkar04@gmail.com>
2025-08-16 21:13:05 +1000

    use fetch to get html of a website

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-npm install
-npm run test
+Install dependencies:
+npm install / npm i
+
+Run tests (vitest)
+npm run test / npm test
+
+
+Run crawl
+npm run start / npm start
--- src/crawl.ts
+++ src/crawl.ts
@@ -1,6 +1,21 @@
 import { JSDOM } from 'jsdom'
 import { argv } from 'node:process'; // first arg - pathname of executable, second arg - path to js file being executed, 3+ -- provided by user
 
+export const getHTML = async (url : string) => {
+    const res = await fetch(url);
+    
+    if (res.status > 400 ) {
+        console.log("Error");
+        return;
+    } else if (!res.headers.get("content-type")!.includes("text/html")) {
+        console.log("Error");
+        return;
+    } else {
+            //const dom = new JSDOM(await res.text());
+            //console.log(dom.window.document.querySelector("body"))
+            console.log(await res.text())
+    }
+}
 
 export const getURLsFromHTML = (html : string, baseURL : string) : string[] => {
     const dom = new JSDOM(html);
@@ -39,14 +54,18 @@ export const normalizeURL = (url: string): string => {
   );
 };
 
-const main = () => {
+const main = async () => {
+
     if(argv.length === 3) {
         console.log(argv[2]);
-        process.exit(0);
+        //process.exit(0);
     } else {
         process.exit(1);
     }
 
+    const BASE_URL = argv[2];
+
+    await getHTML(BASE_URL)
 };
 
 main();
